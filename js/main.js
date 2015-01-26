70f3c1f5737fc88f1ebc7bd2be1fe81a2175f9f0
baerdaniel <dbaer@Daniel-Bar.local>
2015-01-26 21:57:53 +0000

    hiding animation
    changed delays on hide animation

--- js/main.js
+++ js/main.js
@@ -1,19 +1,8 @@
 $(document).ready(function(){
 
-
-/*make .hide transparent*/
-	/*$('.hide').css("opacity", "0");*/
-	
-
-/*WORKING: make .hide opaque 1s*/
-
-/*  	$(".hide").click(function(){
-    	$(".hide").animate({opacity:"1"}, 1000);
-  	});*/
-
 /*make .hide opaque, then fade in after 1s*/
 
-	$(".hide").css("opacity", "0").delay(100).animate({opacity:"1"}, 2000);
+	$(".hide").css("opacity", "0").delay(1500).animate({opacity:"1"}, 2000);
 
 })
 
