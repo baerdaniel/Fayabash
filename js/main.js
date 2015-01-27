2978e111e1af375822cc860cb15234f481673f23
baerdaniel <dbaer@Daniel-Bar.local>
2015-01-27 22:42:52 +0000

    Gallery
    Gallery image placement and delays of fading paragraphs

--- js/main.js
+++ js/main.js
@@ -2,7 +2,18 @@ $(document).ready(function(){
 
 /*make .hide opaque, then fade in after 1s*/
 
-	$(".hide").css("opacity", "0").delay(1500).animate({opacity:"1"}, 2000);
+/*	$(".hide.first").css("opacity", "0").delay(1000).animate({opacity:"1"}, 2000);
+	$(".hide.second").css("opacity", "0").delay(1500).animate({opacity:"1"}, 2000);
+	$(".hide.third").css("opacity", "0").delay(2000).animate({opacity:"1"}, 2000);
+	$(".hide.fourth").css("opacity", "0").delay(2500).animate({opacity:"1"}, 2000);*/
+
+	var timer == 500
+
+	$(".hide").css("opacity", "0");
+	$('.hide.first').delay(1000).animate({opacity:"1"}, 2000);
+	$(".hide.second").delay(1500).animate({opacity:"1"}, 2000);
+	$(".hide.third").delay(2000).animate({opacity:"1"}, 2000);
+	$(".hide.fourth").delay(2500).animate({opacity:"1"}, 2000);
 
 })
 
