a2199955772e61712b30e1bade1ac080e03762b4
baerdaniel <dbaer@Daniel-Bar.local>
2015-01-27 23:57:02 +0000

    logo
    added logo

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
 	$(".hide.third").css("opacity", "0").delay(2000).animate({opacity:"1"}, 2000);
 	$(".hide.fourth").css("opacity", "0").delay(2500).animate({opacity:"1"}, 2000);*/
 
-	var timer == 500
+	var timer = '500';
 
 	$(".hide").css("opacity", "0");
-	$('.hide.first').delay(1000).animate({opacity:"1"}, 2000);
-	$(".hide.second").delay(1500).animate({opacity:"1"}, 2000);
-	$(".hide.third").delay(2000).animate({opacity:"1"}, 2000);
-	$(".hide.fourth").delay(2500).animate({opacity:"1"}, 2000);
+	$('.hide.first').delay(timer * 2).animate({opacity:"1"}, 2000);
+	$(".hide.second").delay(timer * 3).animate({opacity:"1"}, 2000);
+	$(".hide.third").delay(timer * 4).animate({opacity:"1"}, 2000);
+	$(".hide.fourth").delay(timer * 7).animate({opacity:"1"}, 2000);
 
 })
 
